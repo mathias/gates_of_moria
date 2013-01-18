3280c66a5f2991d9b592323a79bea5bc017e9f2c
Matt Gauger <matt.gauger@gmail.com>
2013-01-18 15:52:44 -0600

    Test first prompt

--- spec/gates_of_moria/quiz_spec.rb
+++ spec/gates_of_moria/quiz_spec.rb
@@ -9,6 +9,13 @@ module GatesOfMoria
         game = GatesOfMoria::Quiz.new(messenger, 'gates')
         game.start
       end
+
+      it 'should prompt for input' do
+        messenger = mock('messenger').as_null_object
+        messenger.should_receive(:puts).with("Let me know your name by assigning it to a variable called 'name'!")
+        game = GatesOfMoria::Quiz.new(messenger, 'gates')
+        game.start
+      end
     end
   end
 end
